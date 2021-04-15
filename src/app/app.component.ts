6d748500b1c8e0e08aa4da8803f9424c045da451
Diallo <diallobis2011@gmail.com>
2021-04-15 19:31:02 +0200

    add firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyCNV6CdG8oOeaMQNgQEKZrUbd7QhAXUZoA",
+      authDomain: "bookshelves-eec51.firebaseapp.com",
+      projectId: "bookshelves-eec51",
+      storageBucket: "bookshelves-eec51.appspot.com",
+      messagingSenderId: "931706964657",
+      appId: "1:931706964657:web:8dc769be9e7a8e40e9f090"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
